175d5cbccdeacca3717ce67d8a1ed8d414c514fc
Tim Wahrburg <t.wahrburg@gmail.com>
2022-02-19 18:04:07 +0100

    Added Arduino-Writer

---
+++ symmetrical-barnacle/Assets/Scripts/ArduinoWriter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ArduinoWriter : MonoBehaviour
+{
+    
+    /// <summary>
+    /// 
+    /// sign ist bei einer Bewegung nach rechts "1" -> Plattform fährt links hoch.
+    /// sign ist bei Bewegung geradeaus "0" -> Plattform muss runterfahren / unten bleiben.
+    /// sign ist bei einer Bewegung nach links "-1" -> PLattform fährt rechts hoch.
+    /// 
+    /// </summary>
+    /// <param name="sign"></param>
+
+    public void WriteToArduino(int sign)
+    {
+
+    }
+}
--- symmetrical-barnacle/Assets/Scripts/ShipRotation.cs
+++ symmetrical-barnacle/Assets/Scripts/ShipRotation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShipRotation : MonoBehaviour
 {
     private Rigidbody rigidBody;
+    private ArduinoWriter arduinoWriter;
 
     //public float turnDampening = 10;
     public float rotationSpeed = 15;
@@ -20,76 +21,28 @@ public class ShipRotation : MonoBehaviour
     public void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        arduinoWriter = GetComponent<ArduinoWriter>();
     }
 
-    public void UpdateShipRotation(Quaternion shipRot, float shipsign)
+    public void UpdateShipRotation(Quaternion shipRot, int shipsign)
     {
         float shipRotGoal = transform.rotation.eulerAngles.y + shipsign * rotationSpeed * Time.deltaTime;
 
         Quaternion yRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, shipRotGoal, 0), float.PositiveInfinity);
 
-        /*
-        shipRotGoal = (360 + shipRotGoal) % 360;
-        Quaternion yRotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, shipRotGoal, 0), Time.deltaTime * turnDampening);
-
-        float dist = shipRotGoal - shipRot.eulerAngles.y;
-
-        
-        if (Mathf.Abs(dist) < 180)
-        {
-            if (dist < -10)
-            {
-                sign = -1;
-            } else if (dist > 10)
-            {
-                sign = 1;
-            } else
-            {
-                sign = 0;
-            }
-        } else
-        {
-            if (dist < -10)
-            {
-                sign = 1;
-            }
-            else if (dist > 10)
-            {
-                sign = -1;
-            }
-            else
-            {
-                sign = 0;
-            }
-        }
-        */
-
         Quaternion zRotation = CalculateZRotation(shipsign);
 
         Vector3 fullEulerRotation = new Vector3(0, yRotation.eulerAngles.y, zRotation.eulerAngles.z);
         rigidBody.MoveRotation(Quaternion.Euler(fullEulerRotation));
+
+        arduinoWriter.WriteToArduino(shipsign);
+
+
     }
 
     public Quaternion CalculateZRotation(float sign)
     {
         float angleGoal = angleMax * sign;
-        //Debug.Log($"AngleGoal: {angleGoal}");
-
-
-        /*
-        if (sign > 0)
-        {
-            angleGoal = Mathf.Min(transform.rotation.eulerAngles.z + sign * angleSpeed * Time.deltaTime, angleMax);
-        } else if (sign < 0)
-        {
-            angleGoal = Mathf.Max(transform.rotation.eulerAngles.z + sign * angleSpeed * Time.deltaTime, -angleMax);
-        } else
-        {
-            angleGoal = 0;
-        }*/
-        
-
-        //return Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, -angleGoal, 0), float.PositiveInfinity);
 
         return Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, -angleGoal), angleDampening * Time.deltaTime);
     }
